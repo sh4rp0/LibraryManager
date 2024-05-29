a48a785f16d0f52ec6f564fd6c072e5475b72145
Matúš Zápražný <zaprazny.matus@gmail.com>
2024-05-29 20:36:24 +0200

    Project cleanup

---
+++ LibraryManager.Api/Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        [HttpPost("Add")]
+        public IActionResult AddBook(string bookName)
+        {
+            return Ok($"New book added: { bookName }");
+        }
+    }
+}
--- LibraryManager.Api/Program.cs
+++ LibraryManager.Api/Program.cs
@@ -6,31 +6,17 @@ namespace LibraryManager.Api
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                builder.Services.AddControllers();
             }
 
-            app.UseHttpsRedirection();
+            var app = builder.Build();
+            {
+                app.UseHttpsRedirection();
+                app.MapControllers();
 
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+                app.Run();
+            }
         }
     }
 }
