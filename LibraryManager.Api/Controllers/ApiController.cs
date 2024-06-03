cea593a5fca37ceb1c889e478ce8a68814985451
Matúš Zápražný <zaprazny.matus@gmail.com>
2024-06-03 19:42:59 +0200

    login validation

--- LibraryManager.Api/Controllers/ApiController.cs
+++ LibraryManager.Api/Controllers/ApiController.cs
@@ -1,6 +1,7 @@
 ﻿using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace LibraryManager.Api.Controllers;
 
@@ -16,6 +17,16 @@ public class ApiController : ControllerBase
 
     protected IActionResult Problem(List<Error> errors)
     {
+        if (errors.Count is 0)
+        {
+            return Problem();
+        }
+
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
+
         var firstError = errors[0];
 
         var statusCode = firstError.Type switch
@@ -27,6 +38,20 @@ public class ApiController : ControllerBase
         };
 
         return ProblemWithExtensions(errors, statusCode: statusCode, title: firstError.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        foreach (var error in errors)
+        {
+            modelStateDictionary.AddModelError(
+                error.Code,
+                error.Description);
+        }
+
+        return ValidationProblem(modelStateDictionary);
     }
 
     /// <summary>
--- LibraryManager.Application/DependencyInjection.cs
+++ LibraryManager.Application/DependencyInjection.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using ErrorOr;
+using FluentValidation;
+using LibraryManager.Application.Authentication.Commands.Register;
+using LibraryManager.Application.Authentication.Common;
+using LibraryManager.Application.Common.Behaviors;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace LibraryManager.Application;
 
@@ -7,6 +14,12 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+        
+        services.AddScoped(
+            typeof(IPipelineBehavior<,>),
+            typeof(ValidationBehavior<,>));
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         return services;
     }
 }
